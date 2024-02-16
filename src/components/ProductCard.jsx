3e69c71113096d4c0b8b38b746f090019490e8c8
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2024-02-16 13:49:23 +0100

    add products on home page

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ProductCard({ product }) {
+
+  return (
+    <div className='flex flex-col gap-4 bg-white/30 rounded-lg overflow-hidden'>
+      <img src={product.image} alt={product.title} className='w-64 h-64 object-cover' />
+      <div className="p-4">
+        <h3 className='font-semibold text-xl'>{product.title}</h3>
+        <div className="flex justify-between items-center">
+          <p>In stock : {product.quantity}</p>
+          <p className='text-yellow-400 text-xl'>{product.price}€</p>
+        </div>
+        <div className='flex gap-2 mt-4'>
+          <a href={`/product/${product.id}`} className='rounded-lg w-full py-2 px-2 bg-blue-600 text-white'>View product</a>
+          <button className='rounded-lg w-full py-2 bg-green-600 text-white'>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useGetProductsQuery } from '../services/productsApi';
+
+import ProductCard from '../components/ProductCard';
 
 export default function HomeView() {
+  
+  let { data, isLoading } = useGetProductsQuery()
+  
   return (
-    <div>HomeView</div>
-  )
+    <div className='p-28'>
+    <h1 className='text-3xl underline uppercase'>All our products</h1>
+    <div className='flex gap-12 mt-12 flex-wrap'>
+      {
+        !isLoading ?
+        data.map((product) => {
+          return <ProductCard key={product.id} product={product} />
+        }) :
+        <span>Loading...</span>
+      }
+    </div>
+    </div>
+    )
 }
