c86104f0439bee284ae476718407fe11809ac16a
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2024-02-16 13:39:16 +0100

    finish header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='flex gap-12 items-center p-6 w-full border-b border-white'>
+      <h1 className='text-4xl font-bold'>Etherial</h1>
+      <div className="flex gap-4">
+        <a href="/">Home</a>
+        <a href="/cart">Panier </a>
+      </div>
+    </header>
   )
 }
